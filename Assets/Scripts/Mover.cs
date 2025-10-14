21c1a6dfc4c22a9e761e59c00649f6f42ff3af29
Julian McNeill <julesdowork@gmail.com>
2025-10-14 17:15:09 -0400

    Practicing With Methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,10 +7,23 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Use the W, A, S, and D or arrow keys to move the player around.");
+        Debug.Log("Make sure you don't bump into any obstacles!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0;
