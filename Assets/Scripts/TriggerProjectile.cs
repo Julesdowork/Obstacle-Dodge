bad352541f6f10988773e3005df329e25f7f087d
Julian McNeill <julesdowork@gmail.com>
2025-10-15 18:27:27 -0400

    Trigger Projectile

---
+++ Assets/Scripts/TriggerProjectile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TriggerProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject[] projectiles;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            foreach (GameObject projectile in projectiles)
+            {
+                projectile.SetActive(true);
+            }
+        }
+    }
+}
